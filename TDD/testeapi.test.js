f10c4f8ad8bd692ca41add6a439c06d0cff30a4a
marcelinho0938 <marcelinho.sp11@gmail.com>
2023-06-01 19:11:56 -0300

    Códigos arrumados

--- TDD/testeapi.test.js
+++ TDD/testeapi.test.js
@@ -1,15 +1,17 @@
 import{
     assertEquals,
     assertStringIncludes,
-    assertsArrayIncludes,
-    
-} from "http://deno.land/std/testing/asserts.ts"
+    assertArrayIncludes,
+} from "http://deno.land/std/testing/asserts.ts";
 
 let aluno = 'Cróvis José'
 let professores = ['João', 'Lais', 'Davi', 'Bruno Aparecido Cano']
 
-Deno.test("teste de assertions (API)", () => {
+Deno.test("Teste de assertivos (API)", () => {
     assertEquals(aluno, "Cróvis José");
-    assertStringIncludes(aluno, "José");
+    assertStringIncludes(aluno, "José");  // Se a String possui algo parecido (Cróvis José tem José)
+})
 
+Deno.test("Testando array do professor (API)", () => {
+    assertArrayIncludes([])
 })
